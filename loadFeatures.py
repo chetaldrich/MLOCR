a1d2cfb3badad1c17c9eb8d74aba63c74ae8a769
lbiester <laurajpb@gmail.com>
2015-03-11 23:57:20 -0500

    Making features!

---
+++ loadFeatures.py
@@ -0,0 +1,29 @@
+from numpy import *
+from mnist import *
+from util import Counter
+
+# def loadData():
+#     images, labels = load_mnist('training')
+
+def defineFeatures(imageList, n):
+    imageList = imageList[0:]
+    featureList = []
+    for image in imageList:
+        imgFeature = Counter()
+        for i in range(len(image)):
+            for j in range(len(image[i])):
+                if image[i][j] == 0:
+                    imgFeature[(i, j)] = 0
+                else:
+                    imgFeature[(i, j)] = 1
+                # imgFeature[(i, j)] = image[i][j]
+        featureList.append(imgFeature)
+    return featureList
+
+def main():
+    images, labels = load_mnist('training')
+    featureList = defineFeatures(images)
+
+if __name__=="__main__":
+
+    main()
