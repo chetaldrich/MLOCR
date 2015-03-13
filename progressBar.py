6d58127423dec2c6e21be2edd6ef3009c3d4c060
lbiester <laurajpb@gmail.com>
2015-03-12 21:57:00 -0500

    Progress bar to show progress through slow loops

---
+++ progressBar.py
@@ -0,0 +1,37 @@
+import sys
+from time import sleep
+
+class ProgressBar:
+    def __init__(self, width, loopLength, label=""):
+        self.barWidth = width
+        self.loopLength = loopLength
+        self.label = label
+
+
+    def update(self, iteration):
+        if not (self.shouldUpdate(iteration)):
+            return
+
+        finished = int(iteration / (float(self.loopLength) / self.barWidth)) * "-"
+        remaining = int(self.barWidth - (iteration / (float(self.loopLength) / self.barWidth))) * " "
+        percent = int(float(iteration) / self.loopLength * 100)
+
+        sys.stdout.write('\r')
+        sys.stdout.flush()
+
+        sys.stdout.write('{0} [{1}{2}] {3}%'.format(self.label, finished, remaining, percent))
+        sys.stdout.flush()
+
+    def shouldUpdate(self, iteration):
+        if self.barWidth > self.loopLength:
+            return True
+        return iteration % (self.loopLength / self.barWidth) == 0
+
+    def changeLabel(self, newLabel):
+        self.label = newLabel
+
+    def clear(self):
+        sys.stdout.write('\r')
+        sys.stdout.write(' ' * (len(self.label) + 6 + self.barWidth))
+        sys.stdout.write('\r')
+        sys.stdout.flush()
