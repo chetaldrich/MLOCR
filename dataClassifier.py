c3cd692675435a23318c6f13aae0442ff5ca249f
chetaldrich <chetaldrich@gmail.com>
2015-03-14 16:32:20 -0500

    Init neural net

--- dataClassifier.py
+++ dataClassifier.py
@@ -3,6 +3,7 @@ import perceptron
 import loadFeatures
 import argparse
 import time
+import neuralnet
 
 
 def readCommand():
@@ -11,20 +12,26 @@ def readCommand():
     numTestValues = 100
     numTrainValues = 100
     pixels = 0
+    numNeurons = 30
+    batchSize = 10
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-c", type = str, choices=["naivebayes", "perceptron"], help="selects the classifier for use with the MNIST data")
+    parser.add_argument("-c", type = str, choices = ["naivebayes", "perceptron", "neuralnet"], help = "selects the classifier for use with the MNIST data")
 
-    parser.add_argument("-a", action="store_true", default=False, help="tune while training")
+    parser.add_argument("-a", action = "store_true", default=False, help = "tune while training")
 
-    parser.add_argument("-u", action="store_true", default=False, help="use pre-learned weights for perceptron")
+    parser.add_argument("-u", action = "store_true", default=False, help = "use pre-learned weights for perceptron")
 
-    parser.add_argument("--pixels", type = int, choices=range(13), help="remove this many pixels from outside of photo for faster training")
+    parser.add_argument("--pixels", type = int, choices = range(13), help = "remove this many pixels from outside of photo for faster training")
 
-    parser.add_argument("--train", type = int, help="selects the number of training data samples to be used by the classifier")
+    parser.add_argument("--train", type = int, help = "selects the number of training data samples to be used by the classifier")
 
-    parser.add_argument("--test", type = int,  help="selects the number of testing data samples to be used by the classifier")
+    parser.add_argument("--test", type = int,  help = "selects the number of testing data samples to be used by the classifier")
+
+    parser.add_argument("-n", type = int, help = "designates the number of neurons in the hidden layer of the neural net")
+
+    parser.add_argument("-b", type = int, help = "selects the batch size for the stochastic gradient descent algorithm in the neural net")
 
     args = parser.parse_args()
 
@@ -39,6 +46,8 @@ def readCommand():
         runNaiveBayes(numTrainValues, numTestValues, pixels, args.a)
     elif args.c == "perceptron":
         runPerceptron(numTrainValues, numTestValues, pixels, args.a, args.u)
+    elif args.c == "neuralnet":
+        runNeuralNet(numTrainValues, numTestValues, numNeurons, batchSize)
 
 def runPerceptron(numTrainValues, numTestValues, pixels, tune, useTrainedWeights):
     t = time.clock()
@@ -79,7 +88,9 @@ def runNaiveBayes(numTrainValues, numTestValues, pixels, tune):
     naiveBayesClassifier.test(testingData, testingLabels)
 
 def test(classifiedData, testingLabels, ):
-    #
+    pass
+
+def runNeuralNet(numTrainValues, numTestValues, numNeurons, batchSize):
     pass
 
 
---
+++ neuralnet.py
@@ -0,0 +1,8 @@
+import util
+from loadFeatures import *
+
+
+class NeuralNet:
+
+    def __init__(self, neuronLayerSizes):
+        pass
